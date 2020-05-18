57413903c571fa747db3b15f505ad2d167cdb31a
Kyle Bennett <kyle@slik.com.au>
2020-05-18 17:19:49 +1000

    404

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,10 +1,11 @@
 import React from "react"
 
 const NotFoundPage = () => (
-  <div>
+  <div style={{padding:20}}>
     <h1>NOT FOUND</h1>
     <p>You just hit a page that doesn&#39;t exist... the sadness.</p>
     <div>
+      <iframe src="https://giphy.com/embed/9J7tdYltWyXIY" width="280" height="280" frameBorder="0"></iframe>
     </div>
   </div>
 )
